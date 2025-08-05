7fddd290e1cc4d1534fe48b5956a52f7cf842d85
fabio-lasalvia <99scorpione@gmail.com>
2025-08-05 18:42:20 +0200

    Configurazione per deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test:{
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.js']
+  base: "/30-NomeProgetto-EpiBooks/",
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.js"],
   },
-})
+});
